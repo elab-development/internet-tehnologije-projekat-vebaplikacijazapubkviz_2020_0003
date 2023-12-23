0b874bc761e2fbec0d97dc6c7e5f60839fcc739b
AndjelijaVojnovic <andjelijavojnovic@gmail.com>
2023-12-23 15:38:26 +0100

    Dodata komponenta navigacije

---
+++ src/komponente/Navigacija.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Container, Nav, Navbar} from "react-bootstrap";
+
+const Navigacija = () => {
+    return (
+        <>
+            <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="/">Pub quiz</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                        <Nav.Link href="/events">Events</Nav.Link>
+                        <Nav.Link href="/teams">Teams</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Navigacija;
