1b36513d3d2a428cd1a65a04013692995e90e93a
Ivana Vukmirov <ivanav32001@gmail.com>
2024-01-03 23:39:39 +0100

    Paginacija i filtriranje-sort timova

---
+++ src/components/MyButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MyButton = ({ onClick, label, disabled }) => {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      style={{
+        padding: '10px',
+        fontSize: '16px',
+        cursor: 'pointer',
+        backgroundColor: disabled ? '#b0b0b0' : '#87CEFA', 
+        color: '#fff', 
+        border: 'none',
+        borderRadius: '4px',
+        margin:'20px'
+      }}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default MyButton;
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Team from './Team';
 import axios from 'axios';
-
+import MyButton from '../components/MyButton';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
 const Teams = () => {
 
     let [teams, setTeams] = useState();
-
+    const [currentPage, setCurrentPage] = useState(1);
+    const [teamsPerPage] = useState(5);  
+    const [sortOrder, setSortOrder] = useState('asc');
+    
     useEffect(() => {
       if (teams == null) {
         console.log("jeste");
@@ -21,17 +24,44 @@ const Teams = () => {
       }
     },[teams]);
 
+    const handleSortAndPaginate = () => {
+      const sortedTeams = [...teams];
+      sortedTeams.sort((a, b) => {
+        return sortOrder === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+      });
+  
+      const indexOfLastTeam = currentPage * teamsPerPage;
+      const indexOfFirstTeam = indexOfLastTeam - teamsPerPage;
+      const paginatedTeams = sortedTeams.slice(indexOfFirstTeam, indexOfLastTeam);
+      
+
+      return paginatedTeams;
+    };
+
+    
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const goToFirstPage = () => {
+    setCurrentPage((prevPage) => 1);
+  };
+  
     return (
+      <div>
+        <MyButton label={"Sledećih 5 timova"} onClick={handleNextPage} disabled={false} />
+        <MyButton label={"Početak"} onClick={goToFirstPage} disabled={false} />
+
         <div className="all-teams">
         {teams == null ? (
         <>
          </>
       ) : (
-        teams.map((team) => <Team team={team} key={team.id}/>)
+        handleSortAndPaginate().map((team) => <Team team={team} key={team.id}/>)
       )}
 
    </div>
-    );
+   </div>)
 };
 
 export default Teams;
