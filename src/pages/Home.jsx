5e6775a7b4814a28b3cd8ea13bd415bb8aad6e8a
Ivana Vukmirov <ivanav32001@gmail.com>
2024-01-27 22:32:31 +0100

    Dodate funkcionalnosti za admina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import MyButton from '../components/MyButton';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
+import InputForm from '../components/InputForm';
 
 const Home = ({addToken,token}) => {
   let navigate = useNavigate();
@@ -69,15 +70,129 @@ const Home = ({addToken,token}) => {
       generateRandomFact();
     }, []); 
   
+    const [memberData, setMemberData] = useState({
+      id:"",
+      first_name: "",
+      last_name: "",
+    });
+  
+    function handleInput(e) {
+      let newMemberData = memberData;
+      newMemberData[e.target.name] = e.target.value;
+      setMemberData(newMemberData);
+    }
+
+    function deleteMember(){
+      let config = {
+        method: 'delete',
+        url: 'api/members/'+memberData.id+'/delete',
+        headers: {
+          Authorization: "Bearer " + window.sessionStorage.getItem("auth_token"),
+        }
+      };
+      
+      axios.request(config)
+      .then((response) => {
+        alert("Successfully deleted member!");
+            })
+      .catch((error) => {
+        console.log(token)
+        alert("Error while deleting member")
+      });
+    }
+
+    function updateMember(){
+      let data=JSON.stringify({
+        "first_name": memberData.first_name,
+        "last_name": memberData.last_name
+      });
+      let config = {
+        method: 'put',
+        url: 'api/members/'+memberData.id+'/update',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: "Bearer " + window.sessionStorage.getItem("auth_token"),
+        },
+        data :data
+      };
+      
+      axios.request(config)
+      .then((response) => {
+        alert("Successfully updated member!");
+            })
+      .catch((error) => {
+        console.log(token)
+        alert("Error while updating member")
+      });
+    }
+
+    function insertMember(){
+      let data=JSON.stringify({
+        "first_name": memberData.first_name,
+        "last_name": memberData.last_name
+      });
+      console.log("Ovde "+data);
+      let config = {
+        method: 'post',
+        url: 'api/members/insert',
+        headers: { 
+          'Content-Type': 'application/json',
+          Authorization: "Bearer " + window.sessionStorage.getItem("auth_token"),
+        },
+        data: data
+      };
+      axios.request(config)
+        .then((response) => {
+          alert("Successfully inserted member!");
+          console.log(response)
+              })
+        .catch((error) => {
+          console.log(token)
+          alert("Error while inserting member")
+        });
+    }
 
     return (
       
         <div>
            {window.sessionStorage.getItem("auth_token")===null?
-            (<Login addToken={addToken} />) : (<div><MyButton label={"Logout"} onClick={handleLogout}/>
+            (<Login addToken={addToken} />) : (<div>
+              
+              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+              <div className="card" style={{width:500}}>
+              
+            <div className="card-header">
+                Member
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item"> 
+                  <MyButton label={"Delete member"} disabled={false} onClick={deleteMember} ></MyButton></li>
+                
+                      
+                      <InputForm type={"name"} name={"id"} id={"form4Example7cg"} field={"Member ID:"}
+                        func={(e)=>handleInput(e)}/>
+
+                       <InputForm type={"name"} name={"first_name"} id={"form4Example5cg"} field={"First name"}
+                        func={(e)=>handleInput(e)}/>
+                      
+                      <InputForm type={"name"} name={"last_name"} id={"form4Example6cg"} field={"Last name"}
+                        func={(e)=>handleInput(e)}/>
+                        <MyButton label={"Insert member"} disabled={false} onClick={insertMember}></MyButton>
+                        <MyButton label={"Update member"} disabled={false} onClick={updateMember}></MyButton>
+                        
+              </ul>
+              </div>
+        </div>
+
+              
             <p>Random question:  <span dangerouslySetInnerHTML={{ __html: randomQuestion }} /></p>
              <MyButton label={"Random question:"} disabled={false} onClick={generateRandomQuestion}></MyButton>
-              <p>{randomFact}</p></div>)}
+              <p>{randomFact}</p>
+              <MyButton label={"Logout"} onClick={handleLogout}/>
+
+
+              </div>
+              )}
             
         </div>
         
